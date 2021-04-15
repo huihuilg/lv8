5de7725316355d806e1374afe4d6e8677dbbf4d7
hui <huihuilg@outlook.com>
2021-04-15 19:15:57 +0800

    fix:delete jetsteam

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,22 +3,12 @@
 namespace App\Models;
 
 use App\Models\Traits\Filterable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Fortify\TwoFactorAuthenticatable;
-use Laravel\Jetstream\HasProfilePhoto;
-use Laravel\Sanctum\HasApiTokens;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable implements JWTSubject
 {
-//    use HasApiTokens;
-//    use HasFactory;
-//    use HasProfilePhoto;
-//    use Notifiable;
-//    use TwoFactorAuthenticatable;
+
 
     use Filterable;
 
