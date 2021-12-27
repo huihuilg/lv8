320b5d6a9aa39201ed910610f13a6ab9eb7c1276
hui <huihuilg@outlook.com>
2021-12-27 18:50:53 +0800

    fix:2

--- app/Service/BaseService.php
+++ app/Service/BaseService.php
@@ -8,48 +8,7 @@ use Illuminate\Support\Facades\DB;
 
 class BaseService
 {
-    /**
-     * @var $instance
-     */
-    protected static $instance;
-
-    /**
-     * Notes: 单例禁止克隆
-     * User: hui
-     * Date: 2021/4/8
-     * Time: 6:50 下午
-     */
-    private function __clone()
-    {
-
-    }
-
-    /**
-     * 获取单例对象
-     * @return static
-     */
-    public static function instance()
-    {
-        if (!static::$instance instanceof static) {
-            static::$instance = new static(...func_get_args());
-        }
-        return static::$instance;
-    }
 
 
-    /**
-     * 开启打印查询日志
-     */
-    public static function log()
-    {
-        DB::enableQueryLog();
-    }
 
-    /**
-     * 打印
-     */
-    public static function dump()
-    {
-        dd(DB::getQueryLog());
-    }
 }
